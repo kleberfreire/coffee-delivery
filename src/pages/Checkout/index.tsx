63c023ba755b67a6c62a19bfaa1b0d510dd0a60f
Kleber Freire <ch4r4d4@gmail.com>
2022-09-16 22:40:06 -0300

    continuando o fomulario de pedido

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,20 +1,45 @@
 import { MapPinLine } from 'phosphor-react'
-import { WrapperContainer } from './style'
+import {
+  CompleteYourOrder,
+  ConfirmPurchase,
+  FormContainer,
+  FormPurchase,
+  HeaderForm,
+  InputCep,
+  InputComplement,
+  InputNumber,
+  InputRoad,
+  MultiplesInputContainer,
+  WrapperContainer,
+} from './style'
 
 export function Checkout() {
   return (
     <WrapperContainer>
-      <section>
+      <CompleteYourOrder>
         <h1>Complete seu pedido</h1>
-        <div>
-          <div>
-            <MapPinLine size={32} />
+        <FormContainer>
+          <HeaderForm>
+            <MapPinLine size={20} />
             <div>
               <h2>Endereço de Entrega</h2>
               <p>Informe o endereço onde deseja receber seu pedido</p>
             </div>
-          </div>
-        </div>
+          </HeaderForm>
+          <FormPurchase action="">
+            <InputCep type="text" placeholder="cep" />
+            <InputRoad type="text" placeholder="Rua" />
+            <MultiplesInputContainer>
+              <InputNumber type="text" placeholder="Número" />
+              <InputComplement type="text" placeholder="Complemento" />
+            </MultiplesInputContainer>
+          </FormPurchase>
+        </FormContainer>
+      </CompleteYourOrder>
+      <section>
+        <ConfirmPurchase>
+          <h1>Café selecionados</h1>
+        </ConfirmPurchase>
       </section>
     </WrapperContainer>
   )
--- src/pages/Checkout/style.ts
+++ src/pages/Checkout/style.ts
@@ -4,4 +4,88 @@ export const WrapperContainer = styled.div`
   max-width: 70rem;
   width: 100%;
   margin: 0 auto;
+  display: flex;
+  gap: 2rem;
+`
+
+export const CompleteYourOrder = styled.section`
+  & > h1 {
+    color: ${(props) => props.theme['base-subtitle']};
+    font-size: ${(props) => props.theme['title-xs']};
+    font-family: 'Baloo 2', cursive;
+  }
+`
+
+export const HeaderForm = styled.div`
+  display: flex;
+
+  & > svg {
+    color: ${(props) => props.theme['yellow-dark']};
+
+    margin-right: 0.6875rem;
+  }
+
+  & div {
+    width: 33.125rem;
+  }
+
+  & div > h2 {
+    color: ${(props) => props.theme['base-subtitle']};
+    font-size: ${(props) => props.theme['text-regular-m']};
+  }
+
+  & div > p {
+    color: ${(props) => props.theme['base-text']};
+    font-size: ${(props) => props.theme['text-regular-s']};
+  }
+`
+
+export const FormContainer = styled.div`
+  max-width: 40rem;
+  width: 100%;
+  height: 23.25rem;
+  margin-top: 1rem;
+  background: ${(props) => (props) => props.theme['base-card']};
+  border-radius: 6px;
+  padding: 2.5rem;
+  display: flex;
+  flex-direction: column;
+`
+
+const inputBase = styled.input`
+  color: ${(props) => props.theme['base-label']};
+  padding: 0.75rem;
+  background: ${(props) => props.theme['base-input']};
+  border: 2px solid ${(props) => props.theme['base-button']};
+  border-radius: 6px;
+`
+export const FormPurchase = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+
+export const InputCep = styled(inputBase)`
+  width: 12.5rem;
+  margin-top: 2rem;
+`
+
+export const InputRoad = styled(inputBase)`
+  width: 100%;
+  margin-top: 1rem;
+`
+export const InputNumber = styled(inputBase)`
+  width: 12.5rem;
+  margin-top: 1rem;
+  margin-right: 0.75rem;
+`
+export const InputComplement = styled(inputBase)`
+  width: 100%;
+  margin-top: 1rem;
+`
+export const MultiplesInputContainer = styled.div`
+  display: flex;
+`
+
+export const ConfirmPurchase = styled.div`
+  display: flex;
 `
