f2a46f408ff526449f2988b41b139daf8548497e
Kleber Freire <ch4r4d4@gmail.com>
2022-09-20 20:48:20 -0300

    teminando checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -3,11 +3,15 @@ import {
   CreditCard,
   CurrencyDollar,
   MapPinLine,
+  Minus,
   Money,
+  Plus,
+  Trash,
 } from 'phosphor-react'
 import { ButtonPurchase } from './ButtonPurchase'
 import {
   AddressWrapper,
+  ButtonContainer,
   ButtonsPurchaseWrapper,
   CompleteYourOrder,
   ConfirmPurchase,
@@ -22,10 +26,16 @@ import {
   InputNumber,
   InputRoad,
   InputUF,
+  ItemContainer,
   MethodPurchaseWrapper,
   MultiplesInputContainer,
+  QtdContainer,
+  TotalsContainer,
   WrapperContainer,
 } from './style'
+
+import expressoTradicional from '../../assets/coffees/coffee-tradicional.svg'
+import { ButtonRemove } from './ButtonRemove'
 
 export function Checkout() {
   return (
@@ -33,59 +43,84 @@ export function Checkout() {
       <CompleteYourOrder>
         <h1>Complete seu pedido</h1>
         <FormContainer>
-          <form action="">
-            <AddressWrapper>
-              <HeaderAddress>
-                <MapPinLine size={20} />
-                <div>
-                  <h2>Endereço de Entrega</h2>
-                  <p>Informe o endereço onde deseja receber seu pedido</p>
-                </div>
-              </HeaderAddress>
-              <InputCep type="text" placeholder="cep" />
-              <InputRoad type="text" placeholder="Rua" />
-              <MultiplesInputContainer>
-                <InputNumber type="text" placeholder="Número" />
-                <InputComplementWrapper>
-                  <InputComplement type="text" placeholder="Complemento" />
-                  <span>Opcional</span>
-                </InputComplementWrapper>
-              </MultiplesInputContainer>
-              <MultiplesInputContainer>
-                <InputDistrict type="text" placeholder="Bairro" />
-                <InputCity type="text" placeholder="Cidade" />
-                <InputUF type="text" placeholder="UF" />
-              </MultiplesInputContainer>
-            </AddressWrapper>
-            <MethodPurchaseWrapper>
-              <HeaderPurchase>
-                <CurrencyDollar size={20} />
-                <div>
-                  <h2>Pagamento</h2>
-                  <p>
-                    O pagamento é feito na entrega. Escolha a forma que deseja
-                    pagar
-                  </p>
-                </div>
-              </HeaderPurchase>
-              <ButtonsPurchaseWrapper>
-                <ButtonPurchase
-                  icon={<CreditCard size={32} />}
-                  method="Cartão de crédito"
-                />
-                <ButtonPurchase
-                  icon={<Bank size={32} />}
-                  method="cartão de débito"
-                />
-                <ButtonPurchase icon={<Money size={32} />} method="dinheiro" />
-              </ButtonsPurchaseWrapper>
-            </MethodPurchaseWrapper>
-          </form>
+          <AddressWrapper>
+            <HeaderAddress>
+              <MapPinLine size={20} />
+              <div>
+                <h2>Endereço de Entrega</h2>
+                <p>Informe o endereço onde deseja receber seu pedido</p>
+              </div>
+            </HeaderAddress>
+            <InputCep type="text" placeholder="cep" />
+            <InputRoad type="text" placeholder="Rua" />
+            <MultiplesInputContainer>
+              <InputNumber type="text" placeholder="Número" />
+              <InputComplementWrapper>
+                <InputComplement type="text" placeholder="Complemento" />
+                <span>Opcional</span>
+              </InputComplementWrapper>
+            </MultiplesInputContainer>
+            <MultiplesInputContainer>
+              <InputDistrict type="text" placeholder="Bairro" />
+              <InputCity type="text" placeholder="Cidade" />
+              <InputUF type="text" placeholder="UF" />
+            </MultiplesInputContainer>
+          </AddressWrapper>
+          <MethodPurchaseWrapper>
+            <HeaderPurchase>
+              <CurrencyDollar size={20} />
+              <div>
+                <h2>Pagamento</h2>
+                <p>
+                  O pagamento é feito na entrega. Escolha a forma que deseja
+                  pagar
+                </p>
+              </div>
+            </HeaderPurchase>
+            <ButtonsPurchaseWrapper>
+              <ButtonPurchase
+                icon={<CreditCard size={32} />}
+                method="Cartão de crédito"
+              />
+              <ButtonPurchase
+                icon={<Bank size={32} />}
+                method="cartão de débito"
+              />
+              <ButtonPurchase icon={<Money size={32} />} method="dinheiro" />
+            </ButtonsPurchaseWrapper>
+          </MethodPurchaseWrapper>
         </FormContainer>
       </CompleteYourOrder>
       <section>
         <ConfirmPurchase>
           <h1>Café selecionados</h1>
+          <TotalsContainer>
+            <ItemContainer>
+              <img src={expressoTradicional} alt="" />
+              <div>
+                <h2>Expresso Tradicional</h2>
+                <ButtonContainer>
+                  <QtdContainer>
+                    <button>
+                      <Minus size={12} weight="bold" />
+                    </button>
+                    <input
+                      type="number"
+                      placeholder="1"
+                      max="99"
+                      min="1"
+                      maxLength={2}
+                    />
+                    <button>
+                      <Plus size={12} weight="bold" />
+                    </button>
+                  </QtdContainer>
+                  <ButtonRemove icon={<Trash size={12} />} method="remove" />
+                </ButtonContainer>
+              </div>
+              <span>R$ 9,90</span>
+            </ItemContainer>
+          </TotalsContainer>
         </ConfirmPurchase>
       </section>
     </WrapperContainer>
--- src/pages/Checkout/style.ts
+++ src/pages/Checkout/style.ts
@@ -173,5 +173,100 @@ export const ButtonsPurchaseWrapper = styled.div`
 `
 
 export const ConfirmPurchase = styled.div`
+  & > h1 {
+    color: ${(props) => props.theme['base-subtitle']};
+    font-size: ${(props) => props.theme['title-xs']};
+    font-family: 'Baloo 2', cursive;
+  }
+`
+
+export const TotalsContainer = styled.div`
+  max-width: 28rem;
+  width: 100%;
+  margin-top: 0.9375rem;
+  border-radius: 6px;
+  padding: 2.5rem;
   display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  background: ${(props) => (props) => props.theme['base-card']};
+`
+
+export const ItemContainer = styled.div`
+  display: flex;
+  width: 23rem;
+  & > img {
+    width: 4rem;
+    margin-right: 1.25rem;
+  }
+
+  & > div {
+    & > h2 {
+      font-size: ${(props) => props.theme['text-regular-m']};
+      color: ${(props) => props.theme['base-subtitle']};
+    }
+  }
+
+  & > span {
+    width: 100%;
+    text-align: right;
+    color: ${(props) => props.theme['base-text']};
+    font-size: ${(props) => props.theme['text-bold-m']};
+    font-weight: bold;
+  }
+`
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  margin-top: 0.5rem;
+`
+
+export const QtdContainer = styled.div`
+  width: 4.5=rem;
+  height: 2rem;
+  background: ${(props) => props.theme['base-button']};
+
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  border-radius: 6px;
+
+  margin-right: 0.5rem;
+
+  button {
+    background: none;
+    border: none;
+    width: 1.4rem;
+    height: 100%;
+    cursor: pointer;
+    color: ${(props) => props.theme.purple};
+  }
+
+  & > input[type='number'] {
+    width: 1.2rem;
+    height: 100%;
+    -moz-appearance: textfield;
+    border: none;
+    background: transparent;
+    text-align: center;
+    color: ${(props) => props.theme['base-title']};
+    font-size: ${(props) => props.theme['text-regular-m']};
+  }
+
+  & > input[type='number']::placeholder {
+    color: ${(props) => props.theme['base-title']};
+    font-size: ${(props) => props.theme['text-regular-m']};
+  }
+
+  /* & > input[type='number']:focus {
+    background: #fff;
+  } */
+
+  & > input[type='number']::-webkit-outer-spin-button,
+  input[type='number']::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
 `
